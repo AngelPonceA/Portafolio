610d96ba552c3c863371762d1374b07c26c26dfd
Angel <jos.poncea@duocuc.cl>
2025-04-19 23:22:19 -0400

    Modelos firestore

---
+++ src/app/models/proveedor.models.ts
@@ -0,0 +1,8 @@
+export interface Proveedor {
+    id: string;
+    contacto: string;
+    nombre: string;
+    email: string;
+    telefono: string;
+    tiempo_entrega: number;
+}
---
+++ src/app/models/variante.models.ts
@@ -0,0 +1,8 @@
+export interface Variante {
+    id: string;
+    atributo: string;
+    precio: number;
+    producto_id: string;
+    sku: string;
+    stock: number;
+}
