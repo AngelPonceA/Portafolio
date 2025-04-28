cc2a4ce0f58740ad69c45d26adb9a2fae672dbbe
SeDelafuente <delafuente.sebastian2000@gmail.com>
2025-04-28 08:06:44 -0400

    usuario-vendedor y mis-productos pages

--- src/app/pages/mis-productos/mis-productos-routing.module.ts
+++ src/app/pages/mis-productos/mis-productos-routing.module.ts
@@ -1,12 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-import { NotfoundPage } from './notfound.page';
+import { MisProductosPage } from './mis-productos.page';
 
 const routes: Routes = [
   {
     path: '',
-    component: NotfoundPage
+    component: MisProductosPage
   }
 ];
 
@@ -14,4 +14,4 @@ const routes: Routes = [
   imports: [RouterModule.forChild(routes)],
   exports: [RouterModule],
 })
-export class NotfoundPageRoutingModule {}
+export class MisProductosPageRoutingModule {}
