fbbcbca7fb75de5cf91e1703027b77b1e5130fb8
SeDelafuente <delafuente.sebastian2000@gmail.com>
2025-05-14 06:54:34 -0400

    Se implementa funcionalidad oferta (crud), se modifica eliminarProducto según necesidad

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -14,14 +14,20 @@ import { getFunctions, provideFunctions } from '@angular/fire/functions';
 import { getMessaging, provideMessaging } from '@angular/fire/messaging';
 import { getStorage, provideStorage } from '@angular/fire/storage';
 import { environment } from 'src/environments/environment';
+import { LOCALE_ID } from '@angular/core';
+import localeEsCl from '@angular/common/locales/es-CL';
+import { registerLocaleData } from '@angular/common';
 
+registerLocaleData(localeEsCl);
 @NgModule({
   declarations: [AppComponent,],
   imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule, CommonModule,],
   providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }, NativeStorage, 
     provideFirebaseApp(() => initializeApp(environment.firebase)), 
     provideAuth(() => getAuth()), provideFirestore(() => getFirestore()), provideDatabase(() => getDatabase()), 
-    provideFunctions(() => getFunctions()), provideMessaging(() => getMessaging()), provideStorage(() => getStorage())],
+    provideFunctions(() => getFunctions()), provideMessaging(() => getMessaging()), provideStorage(() => getStorage()),
+    { provide: LOCALE_ID, useValue: 'es-CL' },
+  ],
   bootstrap: [AppComponent],
   schemas: [CUSTOM_ELEMENTS_SCHEMA],
 })
