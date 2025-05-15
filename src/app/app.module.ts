f2a1a6ecd5eff976622214a998878b9b549a556e
SeDelafuente <delafuente.sebastian2000@gmail.com>
2025-05-15 01:57:41 -0400

    Funcionalidad "Editar Producto" agregada a mis-productos.page - Swiper.js implementado en html para vista de imagenes en carrusel en mis-productos.page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -17,7 +17,9 @@ import { environment } from 'src/environments/environment';
 import { LOCALE_ID } from '@angular/core';
 import localeEsCl from '@angular/common/locales/es-CL';
 import { registerLocaleData } from '@angular/common';
+import { register } from 'swiper/element/bundle';
 
+register();
 registerLocaleData(localeEsCl);
 @NgModule({
   declarations: [AppComponent,],
