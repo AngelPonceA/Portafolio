6b1d922369e46489b75809bd065e7f59b5a32732
SeDelafuente <delafuente.sebastian2000@gmail.com>
2025-06-24 22:06:53 -0400

    Fix: Eliminación funcionalidades que no aplicarán a la app

--- src/app/services/soporte/soporte.service.ts
+++ src/app/services/soporte/soporte.service.ts
@@ -27,8 +27,7 @@ export class SoporteService {
 
   // Enviar solicitud de soporte
   async enviarSolicitud(soporte: Omit<solicitudSoporte, 'usuarioId' | 'fechaCreacion'>): Promise<void> {
-    //const usuarioId = await this.authService.getUserId();
-    const usuarioId = 'kCnjHs7m1fWHHnavK2qvv2lRy2L2'
+    const usuarioId = await this.authService.getUserId();
     const uid = doc(collection(this.firestore, 'solicitudes')).id;
     const nuevaSolicitud: solicitudSoporte = {
       ...soporte,
@@ -36,20 +35,6 @@ export class SoporteService {
       fechaCreacion: new Date()
     };
     return setDoc(doc(this.firestore, 'solicitudes', uid), nuevaSolicitud);
-  }
-
-  // Enviar reporte de usuario
-  async enviarReporte(reporteData: Omit<reporte, 'usuarioId' | 'fechaCreacion' | 'estado'>): Promise<void> {
-    //const usuarioId = await this.authService.getUserId();
-    const usuarioId = 'kCnjHs7m1fWHHnavK2qvv2lRy2L2'
-    const uid = doc(collection(this.firestore, 'reportes')).id;
-    const nuevoReporte: reporte = {
-      ...reporteData,
-      usuarioId,
-      fechaCreacion: new Date(),
-      estado: 'pendiente'
-    };
-    return setDoc(doc(this.firestore, 'reportes', uid), nuevoReporte);
   }
 
   // Obtener todas las solicitudes
@@ -84,30 +69,6 @@ export class SoporteService {
     })
   };
 
-  // Obtener todos los reportes
-  async obtenerReportes(): Promise<reporte[]> {
-    const ref = collection(this.firestore, 'reportes');
-    const q = query(ref, orderBy('fechaCreacion', 'desc'));
-    const snapshot = await getDocs(q);
-    return snapshot.docs.map(doc => doc.data() as reporte);
-  }
-
-  // Obtener los reportes con estado == pendiente
-  async ObtenerReportesPendientes(): Promise<reporte[]>{
-    const ref = collection(this.firestore, 'reportes');
-    const q = query(ref, where('estado', '==' , 'pendiente'), orderBy('fechaCreacion', 'desc'))
-    const snapshot = await getDocs(q);
-
-    return snapshot.docs.map(doc => {
-      const data = doc.data();
-      return {
-        ...(data as reporte),
-        id: doc.id,
-        fechaCreacion: data['fechaCreacion']?.toDate ? data['fechaCreacion'].toDate() : data['fechaCreacion']
-      };
-    });
-  };
-
   // Obtener solicitudes de un usuario
   async obtenerSolicitudesPorUsuario(usuarioId: string): Promise<solicitudSoporte[]> {
     const ref = collection(this.firestore, 'solicitudes');
@@ -123,22 +84,9 @@ export class SoporteService {
     });
   }
 
-  // Obtener reportes de un usuario
-  async obtenerReportesPorUsuario(usuarioId: string): Promise<reporte[]> {
-    const ref = collection(this.firestore, 'reportes');
-    const q = query(ref, where('usuarioId', '==', usuarioId), orderBy('fechaCreacion', 'desc'));
-    const snapshot = await getDocs(q);
-    return snapshot.docs.map(doc => doc.data() as reporte);
-  }
-
   // Actualizar estado o prioridad de solicitud
   actualizarSolicitud(id: string, data: Partial<solicitudSoporte>): Promise<void> {
     return updateDoc(doc(this.firestore, 'solicitudes', id), data);
-  }
-
-  // Actualizar estado, prioridad o respuesta del reporte
-  actualizarReporte(id: string, data: Partial<reporte>): Promise<void> {
-    return updateDoc(doc(this.firestore, 'reportes', id), data);
   }
 
   // Recuperar todos los usuarios para página usuarios
