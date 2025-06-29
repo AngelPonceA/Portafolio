3de7efa81e4b605f7f97c54da66d9b5cccc23ef5
SeDelafuente <delafuente.sebastian2000@gmail.com>
2025-06-29 18:34:32 -0400

    refactor: Agurpar páginas soporte en carpeta, recuperación formulario para reportar

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -77,7 +77,11 @@ const routes: Routes = [
   },
   {
     path: 'soporte',
-    loadChildren: () => import('./pages/soporte/soporte.module').then( m => m.SoportePageModule)
+    loadChildren: () => import('./pages/soporte/soporte/soporte.module').then( m => m.SoportePageModule)
+  },
+  {
+    path: 'reporte',
+    loadChildren: () => import('./pages/soporte/reporte/reporte.module').then( m => m.ReportePageModule)
   },
   {
     path: 'admin-menu',
@@ -94,12 +98,11 @@ const routes: Routes = [
   {
     path: 'mis-solicitudes-de-soporte',
     loadChildren: () => import('./pages/mis-solicitudes-de-soporte/mis-solicitudes-de-soporte.module').then( m => m.MisSolucitudesDeSoportePageModule)
-  },  {
+  },
+  {
     path: 'tienda',
     loadChildren: () => import('./pages/tienda/tienda.module').then( m => m.TiendaPageModule)
   },
-
-
 
 ];
 
